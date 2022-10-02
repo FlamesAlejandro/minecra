6d4b0bcca1c897245267e3b050e3f7ac3c806ea3
FlamesAlejandro <flameskazu@gmail.com>
2022-10-01 21:38:53 -0300

    base project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
+import { Ground } from './components/Ground';
+
+function App() {
+  return (
+    <>
+    
+    <Canvas>
+      <Sky sunPosition={[100, 100, 20]}/>
+      <ambientLight intensity={0.5} />
+      <Physics>
+        <Ground></Ground>
+      </Physics>
+    </Canvas>
+    
+    </>
+  );
+}
+
+export default App;
