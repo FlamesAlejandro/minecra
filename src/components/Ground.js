5aa8aee9e08c2f24a5c1fde513f4a24f127f9ac2
FlamesAlejandro <flameskazu@gmail.com>
2022-10-01 21:48:57 -0300

    feature/ground

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,9 +1,19 @@
 import { usePlane } from "@react-three/cannon"
+import { RepeatWrapping } from "three"
 
 export const Ground = () => {
     const [ref] = usePlane(() => ({
-        rotation: [0 ,0 ,0], position: [0, 0, 0]
+        // dejamos el piso a nivel del horizonte, con rotation
+        rotation: [-Math.PI / 4 ,0 ,0], position: [0, 0, 0]
     }))
+
+    // quitar el blooring de la textura, para que se vea pixeleado
+    groundTexture.magFilter = NearestFilter
+    // repetir la textura del piso
+    groundTexture.wrapS = RepeatWrapping
+    groundTexture.wrapT = RepeatWrapping
+    groundTexture.repeat.set(100, 100)
+
 
     return (
         <mesh ref={ref}>
